c318dcae5d700a7306e57c50479b371b4a054468
Sk Asfar Ali <116512658+Asfar35@users.noreply.github.com>
2025-11-10 21:09:18 +0530

    Time: 55 ms (34.85%) | Memory: 29.1 MB (72.50%) - LeetSync

---
+++ 15-3sum/3sum.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        int n = nums.size();
+        vector<vector<int>> ans;
+        sort(nums.begin(), nums.end());
+        for(int i = 0; i<n; i++){
+            int l = i+1, r = n-1;
+            if(i>0 && nums[i] == nums[i-1])continue;
+            while(l<r){
+                int sum = nums[i] + nums[l] + nums[r];
+                if(sum == 0){
+                    ans.push_back({nums[i], nums[l], nums[r]});
+                    while(l<r && nums[l]==nums[l+1] )l++;
+                    while(l<r && nums[r]==nums[r-1])r--;
+                    l++;
+                    r--;
+                }
+                else if(sum<0) l++;
+                else r--;
+            }
+        }
+        // for(int i=0; i<ans.size()-1; i++)
+        //     if(ans[i] == ans[i+1]){
+        //         ans.erase(ans.begin()+i);
+        //         i--;
+        //     }
+
+        // for (auto it = ans.begin(); it + 1 != ans.end();) {
+        //     if (*it == *(it + 1))
+        //         it = ans.erase(it);
+        //     else
+        //         ++it;
+        // }
+
+        // ans.erase(unique(ans.begin(), ans.end()), ans.end());
+
+        return ans;
+    }
+};
