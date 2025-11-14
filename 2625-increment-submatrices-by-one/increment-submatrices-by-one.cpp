52202894de57ae9de3f76fdcd24aebcfe32ccfea
Sk Asfar Ali <116512658+Asfar35@users.noreply.github.com>
2025-11-15 02:23:48 +0530

    Time: 35 ms (56.70%) | Memory: 86.3 MB (99.07%) - LeetSync

--- 2625-increment-submatrices-by-one/increment-submatrices-by-one.cpp
+++ 2625-increment-submatrices-by-one/increment-submatrices-by-one.cpp
@@ -5,8 +5,8 @@ public:
         for(vector<int> &v: queries){
             int col1 = v[1], col2 = v[3];
             for(int i = v[0]; i<= v[2]; i++){
-                ans[i][col1] += 1;
-                if(col2+1 < n) ans[i][col2+1] -= 1;
+                ans[i][col1]++;
+                if(col2+1 < n) ans[i][col2+1]--;
             }
         }
         for(vector<int> &v: ans){
