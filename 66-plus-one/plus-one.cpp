534487778d2cc31928705f245372855f67233931
Sk Asfar Ali <116512658+Asfar35@users.noreply.github.com>
2026-01-01 22:32:48 +0530

    Time: 2 ms (6.69%) | Memory: 11.5 MB (77.72%) - LeetSync

---
+++ 66-plus-one/plus-one.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        int n = digits.size();
+        int i = n-1;
+        while(i>=0 && digits[i] == 9){
+            digits[i--] = 0;
+        }
+        if(i>=0) digits[i]++;
+        else digits.insert(digits.begin(), 1);
+        return digits;
+    }
+};
